dd3e0d602b670535d0c2062cfadb6b2e219c1738
Minjoo <minjooki@andrew.cmu.edu>
2022-04-14 09:58:53 -0400

    more voronoi

--- main.py
+++ main.py
@@ -1,12 +1,9 @@
 from cmu_112_graphics import *
 from plant import *
+from terrain import *
 
 def appStarted(app):
     app.width,app.height = 900,700
-
-    app.charX,app.charY = 650,350
-    app.charHeight = 40
-    app.charWidth = 20
 
     app.openInventory = False
     app.closeInvHeight = 25
@@ -32,71 +29,17 @@ def appStarted(app):
     app.closePlantingHeight = 25
     app.plantingSlot = 75
 
-
-
-    # GARDEN PLANTING GRAPHICS
-    app.treePlotWidth = 700
-    app.treePlotHeight = 200
-
-    # (x0,y0,x1,y1) forms
-    app.treeSpace1 = (0,app.menuButtonHeight,app.treePlotWidth,
-                        app.menuButtonHeight+app.treePlotHeight)
-    app.plotSize = 50
-    app.plotSpacing = 100
-
-    # individual plots to plant trees in
-    app.treeplot1 = (100,175,100+app.plotSize,175+app.plotSize)
-
-    app.treeplot2 = (app.treeplot1[2]+app.plotSpacing,150,
-        app.treeplot1[2]+app.plotSpacing+app.plotSize,150+app.plotSize)
-
-    app.treeplot3 = (app.treeplot2[2]+app.plotSpacing,175,
-        app.treeplot2[2]+app.plotSpacing+app.plotSize,175+app.plotSize)
-
-    app.treeplot4 = (app.treeplot3[2]+app.plotSpacing,125,
-        app.treeplot3[2]+app.plotSpacing+app.plotSize,125+app.plotSize)
-
-    # 'container' of the lower tree plots
-    app.treeSpace2 = (0,app.height-app.treePlotHeight,
-                    app.treePlotWidth,app.height)
-
-    app.pathWidth = 50
-
-    # non-tree plant plots
-    app.plantPlotWidth = 600
-    app.plantPlotHeight = 150
-    app.plantPlot = (75,app.treeSpace1[3]+app.pathWidth,
-      75+app.plantPlotWidth,app.treeSpace1[3]+app.pathWidth+app.plantPlotHeight)
-
-    app.treeplot5 = (100,app.plantPlot[3]+app.pathWidth+100,
-            100+app.plotSize,app.plantPlot[3]+app.pathWidth+100+app.plotSize)
-
-    app.treeplot6 = (app.treeplot5[2]+app.plotSpacing,
-                    app.plantPlot[3]+app.pathWidth+125,
-                    app.treeplot5[2]+app.plotSpacing+app.plotSize,
-                    app.plantPlot[3]+app.pathWidth+125+app.plotSize)
-
-    app.treeplot7 = (app.treeplot6[2]+app.plotSpacing,
-                    app.plantPlot[3]+app.pathWidth+75,
-                    app.treeplot6[2]+app.plotSpacing+app.plotSize,
-                    app.plantPlot[3]+app.pathWidth+75+app.plotSize)
-
-    app.treeplot8 = (app.treeplot7[2]+app.plotSpacing,
-                    app.plantPlot[3]+app.pathWidth+125,
-                    app.treeplot7[2]+app.plotSpacing+app.plotSize,
-                    app.plantPlot[3]+app.pathWidth+125+app.plotSize)
+    # dictionary mapping level to generated terrain
+    app.terrain = makeTerrain(app)
+    app.cellSize = 25
     
-    # list of all the tree plots
-    app.allTreePlots = [app.treeplot1,app.treeplot2,app.treeplot3,app.treeplot4,
-                        app.treeplot5,app.treeplot6,app.treeplot7,app.treeplot8]
+
+def makeTerrain(app):
+    # makes terrain to app.terrain
+    gameHeight = app.height-app.menuButtonHeight
+    voronoiPoints = voronoiSeeds(app.width,gameHeight)
+    return getClosestSeeds(voronoiPoints,app.width,gameHeight)
     
-    app.sidePlot = (app.width-app.plantPlotHeight,65,app.width,
-                    65+app.plantPlotWidth)
-
-
-
-def getInvRowCol(app,event):
-    pass
 
 def isLegalMove(app,dx,dy):
     tempX = app.charX + dx
@@ -146,27 +89,52 @@ def mousePressed(app,event):
         app.openPlanting = False
 
 def redrawAll(app,canvas):
-    drawChar(app,canvas)
+    drawTerrain(app,canvas)
+    # drawChar(app,canvas)
     drawMenuHead(app,canvas)
-    drawPlots(app,canvas)
+    # drawPlots(app,canvas)
 
     if app.openInventory==True:
         drawInventory(app,canvas)
     elif app.openPlanting==True:
         drawPlanting(app,canvas)
 
-def drawChar(app,canvas):
-    x0 = app.charX - app.charWidth/2
-    y0 = app.charY - app.charHeight/2
-    x1 = app.charX + app.charWidth/2
-    y1 = app.charY + app.charHeight/2
-    canvas.create_rectangle(x0,y0,x1,y1,fill="yellow")
+def drawTerrain(app,canvas):
+    # pairs voronoi point to a color
+    colors = ['red','blue','green','yellow','purple','pink','orange']
+    colorPairs = []
+    i = 0
+    for seed in app.terrain:
+        color = colors[i]
+        i += 1
+        colorPairs.append((seed,color))
+    
+    for seedPair in colorPairs:
+        color = seedPair[1]
+        seed = seedPair[0]
+        for (row,col) in app.terrain[seed]:
+            x0 = col*app.cellSize
+            y0 = row*app.cellSize + app.menuButtonHeight
+            x1 = x0 + app.cellSize
+            y1 = y0 + app.cellSize + app.menuButtonHeight
+            canvas.create_rectangle(x0,y0,x1,y1,fill=color)
+
+
+
+
+
+# def drawChar(app,canvas):
+#     x0 = app.charX - app.charWidth/2
+#     y0 = app.charY - app.charHeight/2
+#     x1 = app.charX + app.charWidth/2
+#     y1 = app.charY + app.charHeight/2
+#     canvas.create_rectangle(x0,y0,x1,y1,fill="yellow")
 
 
 def drawMenuHead(app,canvas):
     buttonWidth = 100
     buttonHeight = 40
-    menuItems = ["inventory","plant","water","harvest"]
+    menuItems = ["Inventory","Plant","Water","Harvest"]
     for i in range(4):
         x0 = i*100
         x1 = x0 + app.menuButtonWidth
@@ -211,22 +179,6 @@ def drawInventory(app,canvas):
     
     canvas.create_text(825+app.closeInvHeight/2,50+app.closeInvHeight/2,
                             text='X')
-
-def drawPlots(app,canvas):
-    canvas.create_rectangle(app.treeSpace1[0],app.treeSpace1[1],
-                            app.treeSpace1[2],app.treeSpace1[3],outline="grey")
-
-    canvas.create_rectangle(app.plantPlot[0],app.plantPlot[1],
-                            app.plantPlot[2],app.plantPlot[3])
-
-    canvas.create_rectangle(app.treeSpace2[0],app.treeSpace2[1],
-                            app.treeSpace2[2],app.treeSpace2[3],outline="grey")
-
-    canvas.create_rectangle(app.sidePlot[0],app.sidePlot[1],
-                            app.sidePlot[2],app.sidePlot[3])
-    
-    for plot in app.allTreePlots:
-        canvas.create_rectangle(plot[0],plot[1],plot[2],plot[3])
 
 def drawPlanting(app,canvas):
     marginTop = 200
