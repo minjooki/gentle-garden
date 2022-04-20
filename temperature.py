ad3d6677f3a34868111488b8d64f706967527e15
Minjoo <minjooki@andrew.cmu.edu>
2022-04-20 00:10:22 -0400

    update

---
+++ temperature.py
@@ -0,0 +1,4 @@
+import random
+
+# procedural terrain generation code based off of Kate Weeden:
+# https://medium.com/inspired-to-program-%E3%85%82-%D9%88-%CC%91%CC%91/procedural-generation-in-python-7b75127b2f74 
