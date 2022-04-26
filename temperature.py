a48986bc738e64da527b801f9dd6c9fcdd5930b8
minjooki <102932443+minjooki@users.noreply.github.com>
2022-04-26 15:29:56 -0400

    Delete temperature.py

--- temperature.py
+++
@@ -1,4 +0,0 @@
-import random
-
-# procedural terrain generation code based off of Kate Weeden:
-# https://medium.com/inspired-to-program-%E3%85%82-%D9%88-%CC%91%CC%91/procedural-generation-in-python-7b75127b2f74 
